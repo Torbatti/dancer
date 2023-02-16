4e6cdc297b23b33953f525371489be4645741a97
torbatti <torbattii@gmail.com>
2023-02-16 23:54:00 +0330

    update added color and new line to make it more readable

--- dancer.nim
+++ dancer.nim
@@ -1,3 +1,4 @@
+import std/terminal
 type 
   Dns = object
     name: string
@@ -15,15 +16,25 @@ var dns_list: DnsList
 dns_list = @[default,shecan , online403]
 
 proc dns_changer(dns: Dns) =
-  writeFile("/etc/resolv.conf", dns.dns)
+  writeFile("/etc/resolv.conf", dns.dns) 
+  stdout.styledWriteLine(fgGreen, "Dns Changed to ",dns.name)
+  echo ""
 
-# It only works for the first time and shows the updated resolv.conf info after
-let entireFile = readFile("resolv.conf")
-echo entireFile  # prints the entire file
+proc dns_not_found() =
+  stdout.styledWriteLine(fgRed, "Dns Not Found")
+  echo ""
 
-echo "Choose A Dns"
+
+# # It only works for the first time and shows the updated resolv.conf info after
+# let entireFile = readFile("resolv.conf")
+# echo entireFile  # prints the entire file
+
+echo ""
+echo "Choose A Dns:"
 for i,value in dns_list:
   echo " [",$i,"]",$value.name
+echo ""
+
 
 var chosen_dns = readLine(stdin)
 case chosen_dns
@@ -33,18 +44,17 @@ of "1":
   dns_changer(dns_list[1])
 of "2":
   dns_changer(dns_list[2])
-  echo "Dns Not Found"
 of "3":
   # dns_changer(dns_list[3])
-  echo "Dns Not Found"
+  dns_not_found()
 of "4":
   # dns_changer(dns_list[4])
-  echo "Dns Not Found"
+  dns_not_found()
 of "5":
   # dns_changer(dns_list[5])
-  echo "Dns Not Found"
+  dns_not_found()
 of "6":
   # dns_changer(dns_list[6])
-  echo "Dns Not Found"
+  dns_not_found()
 else:
-  echo "Dns Not Found"
+  dns_not_found()
